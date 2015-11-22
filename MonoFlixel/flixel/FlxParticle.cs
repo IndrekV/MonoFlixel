4d7a2a0dbd010a60f154c0000e867b5b42e8bbff
IndrekV <indzuliin@gmail.com>
2015-11-22 09:50:49 +0200

    Fixed FlxParticle

--- MonoFlixel/flixel/FlxParticle.cs
+++ MonoFlixel/flixel/FlxParticle.cs
@@ -17,13 +17,6 @@ namespace MonoFlixel
 	public class FlxParticle : FlxSprite
 	{
         private Texture2D _texture;
-        
-        /*
-        public FlxPoint minSpeed;
-        public FlxPoint maxSpeed;
-        private float rotateBy;
-        private FlxRect particleRect;
-        */
 
         /// <summary>
         /// How long this particle lives before it disappears.
@@ -47,13 +40,6 @@ namespace MonoFlixel
 		{
 			Lifespan = 0;
 			Friction = 500;
-
-            /*
-            rotateBy = 0;
-            minSpeed = new FlxPoint();
-            maxSpeed = new FlxPoint();
-            particleRect = null;
-            */
 		}
 
         /// <summary>
@@ -117,57 +103,5 @@ namespace MonoFlixel
 		{
 
 		}
-
-        /*
-        /// <summary>
-        /// Internal function used by FlxEmitter to create the particles
-        /// </summary>
-        /// <param name="Graphic"></param>
-        /// <param name="Rotation"></param>
-        /// <param name="Width"></param>
-        /// <param name="Height"></param>
-        /// <returns></returns>
-        public FlxSprite loadParticleGraphic(Texture2D Graphic, float Rotation, float Width = 0, float Height = 0)
-        {
-            rotateBy = Rotation * 0.0174532925f;
-            _texture = Graphic;
-            int numFrames = (int)FlxU.floor(_texture.Width / _texture.Height);
-            
-            if (Width == 0 || Height == 0)
-            {
-                Width = _texture.Height;
-                Height = Width;
-            }
-            frameWidth = width = Width;
-            frameHeight = height = Height;
-            List<FlxRect> _draws = new List<FlxRect>();
-            for (int i = 0; i < numFrames; i++)
-            {
-                FlxRect textureArea = new FlxRect(i * height, 0, width, height);
-                _draws.Add(textureArea);
-            }
-            int di;// = (int)Math.Round(FlxU.randomBetween(0, numFrames));
-            Random r = new Random();
-            di = r.Next(numFrames);
-            particleRect = _draws[di];
-            //FlxRect t = _draws[di];
-            sourceRect = new FlxRect(particleRect.x, particleRect.y, particleRect.width, particleRect.height);
-            return this;
-        }
-        */
-
-        /*
-        public override void draw()
-        {
-            if (visible)
-            {
-                if (_texture != null)
-                {
-                    Rectangle rect = new Rectangle((int)sourceRect.x, (int)sourceRect.y, (int)sourceRect.width, (int)sourceRect.height);
-                    FlxG.spriteBatch.Draw(_texture, getVec2(), rect, _color * alpha, angle, new Vector2(width/2, height/2), scale.getVec2(),  SpriteEffects.None,  0f);
-                }
-            }
-        }
-        */
 	}
 }
